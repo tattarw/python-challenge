23062f81d7fe4985901cf5d67467c7e1a48c3a83
tattarw <61598610+tattarw@users.noreply.github.com>
2020-03-29 20:32:14 -0400

    Add files via upload

---
+++ PyBank/Main.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+#Importing modules and file
+import os
+import csv
+file = os.path.join( os.getcwd(),'Resources','budget_data.csv')
+
+
+# In[15]:
+
+
+#Defining variables
+
+months = 0; 
+totalpl = 0; 
+initialv = 0;
+dif = 0; 
+maxdif = 0;
+mindif = 0; 
+
+
+# In[34]:
+
+
+#Loading csv file
+
+with open(file, newline='') as csvfile:
+     filereader = csv.reader(csvfile, delimiter=',')
+     csv_header = next(filereader)
+    
+     for a in filereader:
+        date = a[0]
+        value = int(a[1])
+        
+        dif = value - initialv
+        
+        if (maxdif<dif):
+            maxdif = dif
+            daxdifd = date
+            
+        if (mindif>dif):
+            mindif = dif
+            mindifd = date 
+            
+        initialv = value
+        months = months + 1
+        totalpl = totalpl + value
+
+
+# In[35]:
+
+
+# Print results
+
+print(f'Financial Analysis')
+print(f'----------------------------')
+print(f'Total Months : {months}')
+print(f'Total: $ {totalpl}')
+print(f'Greatest Increase in Profits: {daxdifd} : ($ {maxdif})')
+print(f'Greatest Decrease in Profits: {mindifd} : ($ {mindif})')
+
+
+# In[ ]:
+
+
+
+
---
+++ PyPoll/Main.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+#Importing modules and file
+
+import os
+import csv
+file = os.path.join(os.getcwd(),'Resources','election_data.csv')
+
+
+# In[53]:
+
+
+#Defining variables
+
+voterid = []
+county = []
+candidates = []
+
+khan = []
+correy = []
+li = []
+otooley = []
+
+
+# In[48]:
+
+
+#Loading csv file
+
+with open(file, newline='') as csvfile:
+    filereader = csv.reader(csvfile, delimiter=',')
+    csv_header = next(filereader)
+
+    for a in filereader:
+        voterid.append(a[0])
+        county.append(a[1])
+        candidates.append(a[2])
+
+    total = (len(voterid))
+
+#Calculating the number of votes 
+
+    for candidate in candidates:
+        
+        if candidate == "Khan":
+            khan.append(candidate)
+            k_votes = len(khan)
+            
+        elif candidate == "Correy":
+            correy.append(candidate)
+            c_votes = len(correy)
+            
+        elif candidate == "O'Tooley":
+            otooley.append(candidate)
+            o_votes = len(otooley)
+            
+        else:
+            li.append(candidate)
+            l_votes = len(li)
+            
+    
+#Percentage of votes for each candidate
+
+    khan_p = round(((k_votes / total) * 100), 2)
+    correy_p = round(((c_votes / total) * 100), 2)
+    li_p = round(((l_votes / total) * 100), 2)
+    otooley_p = round(((o_votes / total) * 100), 2)
+    
+#Determining the winner 
+
+    if khan_p > max(correy_p, li_p, otooley_p):
+        winner = "Khan"
+        
+    elif correy_p > max(khan_p, li_p, otooley_p):
+        winner = "Correy"  
+        
+    elif li_p > max(correy_p, khan_p, otooley_p):
+        winner = "Li"
+        
+    else:
+        winner = "O'Tooley"
+
+
+# In[ ]:
+
+
+# Printing the results to the screen 
+
+print(f'Election Results')
+print(f'----------------------------')
+print(f'Total votes: {total}')
+print(f'Khan: {khan_percent}% ({k_votes})')
+print(f'Correy: {correy_percent}% ({c_votes})')
+print(f'Li: {li_percent}% ({l_votes})')
+print(f'OTooley: {otooley_percent}% ({o_votes})') 
+print(f'-----------------------------------')
+print(f'Winner: {winner}')
+print(f'-----------------------------------')
+
+
+# In[56]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
